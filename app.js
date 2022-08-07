5c47e36e6233715a2735d03c251def048faa8c10
Javo <ing.javiercrm@gmail.com>
2022-08-06 20:40:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+// app y puerto
+const app = express();
+const port = 3000;
+
+// Usos básicos
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+// Conexión a la DB
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'grupogamex'
+});
+
+
+// Home route. hace el conteo de los usuarios
+app.get("/", (req, res)=>{
+    //res.send("Welcome to your first real proyect");
+    //res.sendFile(__dirname + "/index.html");
+    let sql = "select count(*) as conteo from grupogamex.users2";
+    let conteo = 0;
+    
+    connection.query(sql, function(err, results) {
+        if (err) {
+            console.log(err);
+        }
+        conteo = results[0].conteo;
+        //res.send("we have " + conteo + " users in our users table");
+        res.render("index", {usuarios: conteo});
+    });
+});
+
+
+// Post - Registro de user
+app.post("/register", (req, res)=>{
+
+    let nombreUsuario = req.body.nombre;
+    let email = req.body.correo;
+    let sql = "insert into users2 (name, email) values ('" + nombreUsuario +  "', '" + email + "' )";
+
+    connection.query(sql, function(err, results) {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect("/");
+    });
+    
+});
+
+// agrega port dinámico
+app.listen( process.env.PORT || port, ()=>{
+    console.log("Server running a port " + port);
+});
+
+
